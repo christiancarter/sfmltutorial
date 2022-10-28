44330419f0989a5e9f6e433f542ed24852c1f9ce
christiancarter <christian.a.b.carter@gmail.com>
2022-10-28 01:26:02 -0700

    added animation

---
+++ sfmltutorial/Animation.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "SFML\Graphics.hpp"
+
+class Animation
+{
+	private:
+		sf::Vector2u imageCount;
+		sf::Vector2u currentImage;
+		float totalTime;
+		float switchTime;
+	public:
+		Animation(sf::Texture* texture, sf::Vector2u imageCount, float switchTime);
+		~Animation();
+		sf::IntRect uvRect;
+		void Update(int row, float deltaTime);
+};
--- sfmltutorial/main.cpp
+++ sfmltutorial/main.cpp
@@ -1,30 +1,47 @@
-#include "SFML/Graphics.hpp"
+#include "SFML\Graphics.hpp"
+#include "Animation.h"
 #include <iostream>
 
 int main()
 {
 	sf::Event e;
-	sf::RenderWindow w(sf::VideoMode(400, 400), "Test", sf::Style::Close | sf::Style::Resize);
-	sf::CircleShape circle1(75.f);
-	sf::CircleShape circle2(20.f);
+	sf::RenderWindow w(sf::VideoMode(300, 300), "Test", sf::Style::Close | sf::Style::Resize);
+	w.setVerticalSyncEnabled(true);
+	sf::CircleShape circle(100.0f);
+	circle.setPosition(0, -25);
+	circle.setFillColor(sf::Color::Yellow);
 	/*
 		sf::RectangleShape player(sf::Vector2f(static_cast <float> (windowSize.x / 3), static_cast <float> (windowSize.y / 3)));
 	*/
 	sf::Texture playerTexture;
-	playerTexture.loadFromFile("frisk.png");
+	playerTexture.loadFromFile("greenlady.jpg");
+	const int PLAYER_TEXTURE_COLUMNS = 12, PLAYER_TEXTURE_ROWS = 3;
 	sf::Vector2u playerTextureSize = playerTexture.getSize();
-	sf::RectangleShape player(sf::Vector2f(static_cast <float> (playerTextureSize.x / 4), static_cast <float> (playerTextureSize.y)));
-	player.setOrigin(sf::Vector2f(static_cast <float> (playerTextureSize.x / 8), static_cast <float> (playerTextureSize.y / 2)));
+	sf::Vector2u windowSize = w.getSize();
+	sf::RectangleShape player(sf::Vector2f(static_cast <float> (playerTextureSize.x / PLAYER_TEXTURE_COLUMNS),
+		static_cast <float> (playerTextureSize.y / PLAYER_TEXTURE_ROWS)));
+	player.setOrigin(sf::Vector2f(static_cast <float> ((playerTextureSize.x / (PLAYER_TEXTURE_COLUMNS * 2))), 
+		static_cast <float> (playerTextureSize.y / (PLAYER_TEXTURE_ROWS * 2))));
+	player.setPosition(windowSize.x / 2, windowSize.y / 2);
 	player.setTexture(&playerTexture);
+
+	Animation animation(&playerTexture, sf::Vector2u(PLAYER_TEXTURE_COLUMNS, PLAYER_TEXTURE_ROWS), 0.15f);
+	float deltaTime = 0.0f;
+	float totalTime = 0.0f;
+	sf::Clock clock;
+
+	/*
 	sf::Vector2u textureSize = playerTexture.getSize();
-	textureSize.x /= 4;
+	textureSize.x /= 12;
+	textureSize.y /= 3;
+	player.setTextureRect(sf::IntRect(0, 0, textureSize.x, textureSize.y));
+	*/
 
-	player.setTextureRect(sf::IntRect(0, 0, textureSize.x - 1, textureSize.y));
-
-	circle1.setFillColor(sf::Color::Blue);
-	circle2.setFillColor(sf::Color::Yellow);
+	sf::Time elapsedTime = clock.getElapsedTime();
 	while (w.isOpen())
 	{
+		deltaTime = clock.restart().asSeconds();
+		animation.Update(0, deltaTime);
 		while (w.pollEvent(e))
 		{
 			switch (e.type)
@@ -33,10 +50,6 @@ int main()
 					w.close();
 					break;
 				case sf::Event::Resized:
-					/*
-					std::cout << '(' << e.size.width << ", " << e.size.height << ')'
-						<< '\n';
-					*/
 					printf("New window dimensions: %i, %i\n", e.size.width, e.size.height);
 					break;
 				case sf::Event::TextEntered:
@@ -47,32 +60,39 @@ int main()
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
 			{
-				player.move(-2.5f, 0.0f);
+				player.move(-1.0f, 0.0f);
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
 			{
-				player.move(2.5f, 0.0f);
+				player.move(1.0f, 0.0f);
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
 			{
-				player.move(0.0f, -2.5f);
+				player.move(0.0f, -1.0f);
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
 			{
-				player.move(0.0f, 2.5f);
+				player.move(0.0f, 1.0f);
 			}
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 			{
 				sf::Vector2i mousePos = sf::Mouse::getPosition(w);
-				player.setPosition(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+				if (mousePos.x >= 0 && mousePos.x <= windowSize.x && mousePos.y >= 0 && mousePos.y <= windowSize.y)
+				{
+					player.setPosition(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+				}
 			}
 
-			w.clear();
-			w.draw(circle1);
-			w.draw(circle2);
-			w.draw(player);
-			w.display();
 		}
+		//(windowSize.x)
+
+		circle.move(0.1f, -0.0f);
+		// circle1.move(0.01f, -0.01f);
+		player.setTextureRect(animation.uvRect);
+		w.clear(sf::Color(0, 0, 180));
+		w.draw(circle);
+		w.draw(player);
+		w.display();
 	}
 	return 0;
 }
